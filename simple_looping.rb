35c9692d61809fda6bcedd9a4673ad7d6b3df98a
L D <tapircode@gmail.com>
2019-02-26 17:09:38 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -30,7 +30,7 @@ def while_iterator(number_of_times)
 end
 
 def until_iterator(number_of_times)
-  counter = 1 
+  counter = 0 
   until counter == number_of_times
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase 
